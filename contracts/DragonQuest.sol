54e97d956424803f5d43b6f30b2ef2ed90f04baa
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2022-06-27 18:46:46 -0400

    Preparing NFT Game Tutorial

---
+++ contracts/DragonQuest.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract DragonQuest {
+    constructor() {
+        console.log("Prepare to destroy the Dark Lord.");
+    }
+}
